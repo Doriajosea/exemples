363acdab26c6a4b80dabe0bff066d1e59b3a57f4
Doriajosea <nikiemadoriajoseannick@gmail.com>
2023-09-20 14:51:31 -0400

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$team_1_data = ["etalons",3];
+echo $team_1_data[0];
+
+$team_1_data=[
+    "name" => "etalons",
+    "score" => 3
+];
+
+$team_2_data=[
+    "name" => "elephans",
+    "score" => 1,
+];
+
+
+
+?>
